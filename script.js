14636d4f921d84e2f18c1e32b2690bc0ded2b494
ayushh025 <ayushprj12@gmail.com>
2026-04-15 15:57:52 +0530

    done

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const todo = document.querySelector("#to-do");
 const progress = document.querySelector("#progress");
 const done = document.querySelector("#done");
-const tasks = document.querySelectorAll(".task");
-const togglModal = document.querySelector("#toggle-modal");
+const toggleModal = document.querySelector("#toggle-modal");
 const modal = document.querySelector(".modal");
 const bg = document.querySelector(".bg");
 const addTaskBtn = document.querySelector("#add-new-task");
 const errMsgCon = document.querySelector(".err-msg");
+const columns = document.querySelectorAll(".task-column");
 
 let dragElement = null;
+const savedTask = JSON.parse(localStorage.getItem("tasks"));
+const allTasks = {};
 
-tasks.forEach((task) => {
-  task.addEventListener("drag", () => {
-    dragElement = task;
-  });
-});
-togglModal.addEventListener("click", () => {
+// Show saved task
+if (savedTask) {
+  for (const colId in savedTask) {
+    const col = document.querySelector(`#${colId}`);
+    savedTask[colId].forEach((task) => {
+      createTask(task.title, task.desc, col);
+    });
+  }
+}
+
+toggleModal.addEventListener("click", () => {
   modal.classList.add("active");
 });
 bg.addEventListener("click", () => {
@@ -23,43 +30,26 @@ bg.addEventListener("click", () => {
 });
 addTaskBtn.addEventListener("click", () => {
   errMsgCon.innerHTML = "";
-  const taskTitle = document.querySelector("#task-title-input").value;
-  const taskDesc = document.querySelector("#task-desc-input").value;
+  const taskTitle = document.querySelector("#task-title-input");
+  const taskDesc = document.querySelector("#task-desc-input");
   let isValid = true;
 
-  if (taskTitle.trim() === "") {
+  if (taskTitle.value.trim() === "") {
     const titleErr = document.createElement("p");
     titleErr.textContent = "Please Enter Title";
     errMsgCon.append(titleErr);
     isValid = false;
   }
-  if (taskDesc.trim() === "") {
+  if (taskDesc.value.trim() === "") {
     const DescErr = document.createElement("p");
     DescErr.textContent = "Please Enter Description";
     errMsgCon.append(DescErr);
     isValid = false;
   }
   if (isValid) {
-    const task = document.createElement("div");
-    task.draggable = true;
-    task.classList.add("task");
-    const h2 = document.createElement("h2");
-    h2.textContent = taskTitle;
-    const p = document.createElement("p");
-    p.textContent = taskDesc;
-    const btn = document.createElement("button");
-    btn.textContent = "Delete";
-    task.append(h2, p, btn);
-
-    // Delete logic
-    btn.addEventListener("click", () => {});
-
-    // Drag logic
-    task.addEventListener("drag", () => {
-      dragElement = task;
-    });
-
-    todo.append(task);
+    createTask(taskTitle.value, taskDesc.value, todo);
+    taskTitle.value = "";
+    taskDesc.value = "";
     modal.classList.remove("active");
   }
 });
@@ -68,20 +58,72 @@ addDragEff(todo);
 addDragEff(progress);
 addDragEff(done);
 
+function updateCount() {
+  columns.forEach((col) => {
+    const count = col.querySelector(".right");
+    const tasks = col.querySelectorAll(".task");
+    count.textContent = tasks.length;
+
+    allTasks[col.id] = Array.from(tasks).map((t) => {
+      return {
+        title: t.querySelector("h2").textContent,
+        desc: t.querySelector("p").textContent,
+      };
+    });
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(allTasks));
+}
+
+function createTask(taskTitle, taskDesc, column) {
+  const task = document.createElement("div");
+  task.draggable = true;
+  task.classList.add("task");
+  const h2 = document.createElement("h2");
+  h2.textContent = taskTitle;
+  const p = document.createElement("p");
+  p.textContent = taskDesc;
+  const btn = document.createElement("button");
+  btn.textContent = "Delete";
+  task.append(h2, p, btn);
+
+  // Delete logic
+  btn.addEventListener("click", () => {
+    task.remove();
+    updateCount();
+  });
+
+  // Drag logic
+  task.addEventListener("dragstart", () => {
+    dragElement = task;
+  });
+
+  column.append(task);
+  updateCount();
+}
+
 function addDragEff(column) {
   column.addEventListener("dragenter", (e) => {
-    e.preventDefault();
-    column.classList.add("dragover");
+    if (dragElement) {
+      e.preventDefault();
+      column.classList.add("dragover");
+    }
   });
   column.addEventListener("dragleave", (e) => {
-    e.preventDefault();
-    column.classList.remove("dragover");
+    if (dragElement) {
+      e.preventDefault();
+      column.classList.remove("dragover");
+    }
   });
   column.addEventListener("dragover", (e) => {
-    e.preventDefault();
+    if (dragElement) e.preventDefault();
   });
   column.addEventListener("drop", () => {
-    column.classList.remove("dragover");
-    column.append(dragElement);
+    if (dragElement) {
+      column.classList.remove("dragover");
+      column.append(dragElement);
+      updateCount();
+      dragElement = null;
+    }
   });
 }
